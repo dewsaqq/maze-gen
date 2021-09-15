6ba9e0066f5ca49d54303db38ef1ba313f3e84cb
Arek <arekdbr97@gmail.com>
2021-09-15 15:03:50 +0200

    Add refactored recursive backtracker generator + adjustment to GUI maze representation

--- mazegen/src/main/java/maze/Maze.java
+++ mazegen/src/main/java/maze/Maze.java
@@ -27,15 +27,36 @@ public class Maze {
     {
         g.setColor(Color.BLACK);
         for (int i = 0; i < grid.width; i++) {
+            //north
+            g.drawLine((i * CELL_WIDTH + MARGIN), (0 * CELL_WIDTH + MARGIN),
+                    ((i + 1) * CELL_WIDTH + MARGIN), (0 * CELL_WIDTH + MARGIN));
+
+            //south
+            g.drawLine(i * CELL_WIDTH + MARGIN, grid.height * CELL_WIDTH
+                    + MARGIN, (i + 1) * CELL_WIDTH + MARGIN, grid.height * CELL_WIDTH
+                    + MARGIN);
+        }
+        for (int j = 0; j < grid.height; j++) {
+            //east
+            g.drawLine((grid.width) * CELL_WIDTH + MARGIN, j * CELL_WIDTH
+                    + MARGIN, (grid.width) * CELL_WIDTH + MARGIN, (j + 1) * CELL_WIDTH
+                    + MARGIN);
+
+            //west
+            g.drawLine(0 * CELL_WIDTH + MARGIN, j * CELL_WIDTH + MARGIN, 0
+                    * CELL_WIDTH + MARGIN, (j + 1) * CELL_WIDTH + MARGIN);
+        }
+
+        for (int i = 0; i < grid.width; i++) {
             for (int j = 0; j < grid.height; j++) {
-                if (!grid.getCell(j, i).getWall(Wall.NORTH).isOpen()) // if there exists a wall to the
+                if (grid.getCell(j, i).getWall(WallPosition.NORTH) != null && !grid.getCell(j, i).getWall(WallPosition.NORTH).isOpen()) // if there exists a wall to the
                 // north
                 {
                     g.drawLine((i * CELL_WIDTH + MARGIN), (j * CELL_WIDTH + MARGIN),
                             ((i + 1) * CELL_WIDTH + MARGIN), (j * CELL_WIDTH + MARGIN));
                 }
 
-                if (!grid.getCell(j, i).getWall(Wall.SOUTH).isOpen()) // if there exists a wall to the
+                if (grid.getCell(j, i).getWall(WallPosition.SOUTH) != null && !grid.getCell(j, i).getWall(WallPosition.SOUTH).isOpen()) // if there exists a wall to the
                 // south
                 {
                     g.drawLine(i * CELL_WIDTH + MARGIN, (j + 1) * CELL_WIDTH
@@ -43,7 +64,7 @@ public class Maze {
                             + MARGIN);
                 }
 
-                if (!grid.getCell(j, i).getWall(Wall.EAST).isOpen()) // if there exists a wall to the
+                if (grid.getCell(j, i).getWall(WallPosition.EAST) != null && !grid.getCell(j, i).getWall(WallPosition.EAST).isOpen()) // if there exists a wall to the
                 // east
                 {
                     g.drawLine((i + 1) * CELL_WIDTH + MARGIN, j * CELL_WIDTH
@@ -51,7 +72,7 @@ public class Maze {
                             + MARGIN);
                 }
 
-                if (!grid.getCell(j, i).getWall(Wall.WEST).isOpen()) // if there exists a wall to the
+                if (grid.getCell(j, i).getWall(WallPosition.WEST) != null && !grid.getCell(j, i).getWall(WallPosition.WEST).isOpen()) // if there exists a wall to the
                 // west
                 {
                     g.drawLine(i * CELL_WIDTH + MARGIN, j * CELL_WIDTH + MARGIN, i
